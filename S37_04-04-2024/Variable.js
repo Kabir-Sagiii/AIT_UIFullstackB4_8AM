9243e9c10868153ebb27fb4f4bb79e7814b5cdd0
Kabir <sagithedeveloper@gmail.com>
2024-04-04 11:28:57 +0530

    Added the code

---
+++ S37_04-04-2024/Variable.js
@@ -0,0 +1,21 @@
+id;
+
+city = "Hyderabad";
+
+var productName = "Iphone";
+
+let price = 1000;
+
+const pin = 34567;
+
+// var 1s = "" Invalid
+
+//  var @ait = ""
+
+var $name = "lsdnsdklfnsk";
+
+var _gender = "male";
+
+var s12 = "";
+
+//
